b916ff27ecab1f9eb6d447faf8e15857c4617886
08Abhijeet <abhijeetkadam656@gmail.com>
2024-04-05 15:07:21 +0530

    UPDated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,143 +1,144 @@
-import React from 'react'
+import React from "react";
 import { Box, Button, styled, Typography } from "@mui/material";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 //img
-import headerImg from '../assets/pexels-binyamin-mellish-186078.png'
+import headerImg from "../assets/pexels-binyamin-mellish-186078.png";
 
 const Header = () => {
+  const CustomBox = styled(Box)(({ theme }) => ({
+    minHeight: "80vh",
+    display: "flex",
+    justifyContent: "center",
+    // tamanhos
+    gap: theme.spacing(2),
+    paddingTop: theme.spacing(10),
+    // cor de fundo
+    backgroundColor: "#1D24CA",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
+    },
+  }));
 
-    const CustomBox = styled(Box) (({ theme }) => ({
-        minHeight: '80vh',
-        display: 'flex',
-        justifyContent: 'center',
-        // tamanhos
-        gap: theme.spacing(2),
-        paddingTop: theme.spacing(10),
-        // cor de fundo
-        backgroundColor: '#1D24CA',
-        [theme.breakpoints.down('md')]: {
-            flexDirection: 'column',
-            alignItems: 'center',
-            textAlign: 'center',
-        }
-    }));
+  const BoxText = styled(Box)(({ theme }) => ({
+    flex: "1",
+    paddingLeft: theme.spacing(8),
+    [theme.breakpoints.down("md")]: {
+      flex: "2",
+      textAlign: "center",
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+    },
+  }));
 
-    const BoxText = styled(Box) (({ theme }) => ({
-        flex: '1',
-        paddingLeft: theme.spacing(8),
-        [theme.breakpoints.down('md')]: {
-            flex: '2',
-            textAlign: 'center',
-            paddingLeft: theme.spacing(2),
-            paddingRight: theme.spacing(2),
-        },
-    }));
+  return (
+    <CustomBox component="header">
+      {/*  Box text  */}
+      <BoxText component="section">
+        <Typography
+          variant="h2"
+          component="h1"
+          sx={{
+            fontWeight: 700,
+            color: "#fff",
+          }}
+        >
+          WealthWise: Crafting Your Financial Future
+        </Typography>
 
+        <Typography
+          variant="p"
+          component="p"
+          sx={{
+            py: 3,
+            lineHeight: 1.6,
+            color: "#fff",
+          }}
+        >
+          Welcome to WealthWise, your comprehensive financial management
+          platform designed to help you achieve your financial goals and secure
+          your future.
+        </Typography>
 
-  return  (
-        <CustomBox component='header'>
-            {/*  Box text  */}
-            <BoxText 
-            component='section'
-            >
-                <Typography
-                variant='h2'
-                component= 'h1'
-                sx={{
-                    fontWeight: 700,
-                    color: '#fff',
-                }}
-                >
-                   WealthWise: Crafting Your Financial Future
-                </Typography>
+        <Box>
+          <Button
+            variant="contained"
+            component={Link}
+            to="form.html"
+            sx={{
+              mr: 2,
+              px: 4,
+              py: 1,
+              fontSize: "0.9rem",
+              textTransform: "capitalize",
+              borderRadius: 0,
+              borderColor: "#14192d",
+              color: "#fff",
+              backgroundColor: "#14192d",
+              "&&:hover": {
+                backgroundColor: "#343a55",
+              },
+              "&&:focus": {
+                backgroundColor: "#343a55",
+              },
+            }}
+          >
+            Start now
+          </Button>
 
-                <Typography
-                variant='p'
-                component='p'
-                sx={{
-                    py: 3,
-                    lineHeight: 1.6,
-                    color: '#fff',
-                }}
-                >
-                   Welcome to WealthWise, your comprehensive financial management platform designed to help you achieve your financial goals and secure your future.
-                </Typography>
+          <Button
+            component={Link}
+            to={"/about"}
+            variant="outlined"
+            sx={{
+              px: 4,
+              py: 1,
+              fontSize: "0.9rem",
+              textTransform: "capitalize",
+              borderRadius: 0,
+              color: "#fff",
+              backgroundColor: "transparent",
+              borderColor: "#fff",
+              "&&:hover": {
+                color: "#343a55",
+                borderColor: "#343a55",
+              },
+              "&&:focus": {
+                color: "#343a55",
+                borderColor: "#343a55",
+              },
+            }}
+          >
+            explore
+          </Button>
+        </Box>
+      </BoxText>
 
-                <Box>
-                    <Button 
-                    variant='contained'
-                    sx={{
-                        mr: 2,
-                        px: 4, 
-                        py: 1,
-                        fontSize: '0.9rem',
-                        textTransform: 'capitalize',
-                        borderRadius: 0,
-                        borderColor: '#14192d',
-                        color: '#fff',
-                        backgroundColor: '#14192d',
-                        "&&:hover": {
-                            backgroundColor: "#343a55"
-                        },
-                        "&&:focus": {
-                            backgroundColor: "#343a55"
-                        }
-                    }}
-                    >
-                        Start now
-                    </Button>
-                    <Button 
-                    component={Link} 
-                    to={'/about'}
-                    variant='outlined'
-                    sx={{
-                        px: 4, 
-                        py: 1,
-                        fontSize:'0.9rem',
-                        textTransform: 'capitalize',
-                        borderRadius: 0,
-                        color: '#fff',
-                        backgroundColor: 'transparent',
-                        borderColor: '#fff',
-                        "&&:hover": {
-                            color: '#343a55',
-                            borderColor: '#343a55',
-                        },
-                        "&&:focus": {
-                            color: '#343a55',
-                            borderColor: '#343a55',
-                        }
-                    }}
-                    >
-                        explore
-                    </Button>
-                </Box>
-            </BoxText>
+      <Box
+        sx={(theme) => ({
+          [theme.breakpoints.down("md")]: {
+            flex: "1",
+            paddingTop: "30px",
+            alignSelf: "center",
+          },
+          [theme.breakpoints.up("md")]: {
+            flex: "2",
+            alignSelf: "flex-end",
+          },
+        })}
+      >
+        <img
+          src={headerImg}
+          alt="headerImg"
+          style={{
+            width: "100%",
+            marginBottom: -15,
+          }}
+        />
+      </Box>
+    </CustomBox>
+  );
+};
 
-            <Box sx={theme => ({
-                [theme.breakpoints.down('md')]:{
-                    flex: '1',
-                    paddingTop: '30px',
-                    alignSelf: 'center',
-                },
-                [theme.breakpoints.up('md')]:{
-                    flex: '2',
-                    alignSelf: 'flex-end',
-                },
-            })}
-            >
-                <img
-                src={headerImg}
-                alt="headerImg"
-                style={{ 
-                    width: "100%", 
-                    marginBottom: -15,
-                }}
-                />
-            </Box>
-
-        </CustomBox>
-    )
-}
-
-export default Header
+export default Header;
